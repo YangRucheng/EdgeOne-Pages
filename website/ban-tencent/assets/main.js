595d70ccc201f451011ff4592ce71dabbfcbc6ca
Sakana <admin@yangrucheng.top>
2025-09-04 21:39:10 +0800

    feat: add new site

---
+++ website/ban-tencent/assets/main.js
@@ -0,0 +1,9 @@
+const copyLink = () => {
+    const dummy = document.createElement('input');
+    const text = window.location.href;
+    document.body.appendChild(dummy);
+    dummy.value = text;
+    dummy.select();
+    document.execCommand('copy');
+    document.body.removeChild(dummy);
+}
